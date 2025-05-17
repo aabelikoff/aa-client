c5609525ead0efab539e31f3ec78681f21fc1fe4
Oleksii Bielikoff <aabelikoff@gmail.com>
2025-05-17 10:57:27 +0100

    update: withdrawStake

--- src/withdrawStake.js
+++ src/withdrawStake.js
@@ -7,15 +7,19 @@ import {
 } from "@aa-sdk/core";
 import { getUserOperationHash } from "viem/account-abstraction";
 import { arbitrumSepolia } from "viem/chains";
+import dayjs from "dayjs";
+import duration from "dayjs/plugin/duration.js";
+dayjs.extend(duration);
 
 config();
 
 const rpcUrl = `https://arb-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`;
 const OWNER_PRIVATE_KEY = process.env.OWNER_PRIVATE_KEY;
 const PAYMASTER_ADDRESS = process.env.PAYMASTER_CONTRACT_ADDRESS;
+const OLEKSANDR_OWNER_ADDRESS = '0x79f41F1E02C26a28cB7feD0f00D3392CF10528A2';
 
 
-const main = async () => {
+const main = async (receiver) => {
     const provider = new ethers.JsonRpcProvider(rpcUrl);
     
     const network = await provider.getNetwork(); // <-- правильно
@@ -41,17 +45,22 @@ const main = async () => {
     
     if(block.timestamp < depositInfo[4]) {
         console.log("You can't withdraw stake yet. Wait until the withdraw time.");
-        console.log("Try again after: ", Number(depositInfo[4]) - block.timestamp, " seconds");
-        return;
+        const seconds = Number(depositInfo[4]) - Number(block.timestamp);
+        const dur = dayjs.duration(seconds, 'seconds');
+        const h = dur.hours() > 0 ? `${dur.hours()}h ` : '';
+        const m = dur.minutes() > 0 ? `${dur.minutes()}m ` : '';
+        const s = dur.seconds() > 0 ? `${dur.seconds()}s` : '';
+
+        throw new Error(`Try again after: ${h}${m}${s}`.trim());
     }
-    const tx = await paymasterContract.withdrawStakeFromEntryPoint(owner.address);
+    const tx = await paymasterContract.withdrawStakeFromEntryPoint(receiver);
     const receipt = await tx.wait();
     console.log("Transaction hash:", tx.hash);
 
 
 }
 
-main()
+main(OLEKSANDR_OWNER_ADDRESS)
     .then(() => {
         console.log("Stake withdrawn successfully");
     })
